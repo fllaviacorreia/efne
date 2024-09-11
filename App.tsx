c8a597241276efbdb1b38813f9bfd2f324711dee
Flávia Correia <fllaviacorreia@outlook.com>
2024-09-10 22:40:03 -0300

    alter to default

--- App.tsx
+++ App.tsx
@@ -1,9 +1,24 @@
-import AppNavigation from '@/navigation/AppNavigaton';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
 
 function App() {
   return (
-    <AppNavigation />
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
 
 export default App;
