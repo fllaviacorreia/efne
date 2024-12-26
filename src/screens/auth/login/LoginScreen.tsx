df93b2041be6ca6d903e85390a603a7374623991
Flávia Correia <fllaviacorreia@outlook.com>
2024-12-26 16:13:18 -0300

    finish login screen

--- src/screens/auth/login/LoginScreen.tsx
+++ src/screens/auth/login/LoginScreen.tsx
@@ -5,10 +5,11 @@ import { useRef, useState } from "react";
 import { Formik } from "formik";
 import LoginSchema from "@/validators/loginSchema";
 import { useAuth } from "@/context/AuthContext";
-import { Button, CheckBox, Input, Layout, Text } from "@ui-kitten/components";
+import { Button, CheckBox, Input, Layout, Text, useTheme } from "@ui-kitten/components";
 import { Image } from "expo-image";
 import styles from "./styles";
 import { useAssets } from "expo-asset";
+import { KeyboardAvoidingView, Platform, ScrollView } from "react-native";
 
 type loginParamsList = NativeStackNavigationProp<RoutesParamList, "Login">;
 
@@ -20,6 +21,9 @@ export default function LoginScreen() {
 
   const passwordRef = useRef<Input>(null);
 
+  const theme = useTheme();
+
+
   const onSubmitting = async (value: { username: string, password: string, keepConnected: boolean }) => {
     try {
       await login(value);
@@ -27,97 +31,104 @@ export default function LoginScreen() {
     } catch (e) {
       alert("Erro ao logar.")
     }
-    finally{
+    finally {
       setLoading(false)
     }
   }
 
   return (
-    <Layout style={styles.container}>
-      <Layout style={styles.containerImg}>
-        <Image
-          source={assets ? assets[0] : null}
-          contentFit="contain"
-          style={{ width: 150, height: 180 }}
-        />
-      </Layout>
+    <KeyboardAvoidingView
+      style={{flex: 1, backgroundColor: theme['background-basic-color-1'] }}
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+      keyboardVerticalOffset={Platform.OS === 'ios' ? 64 : 0}
+    >
+      <ScrollView
+        keyboardShouldPersistTaps="handled"
+        contentContainerStyle={{ flexGrow: 1 }}
+      >
+        <Layout style={styles.container}>
+        <Layout style={styles.containerImg}>
+          <Image
+            source={assets ? assets[0] : null}
+            contentFit="contain"
+            style={styles.image}
+          />
+        </Layout>
 
-      {/* Container para o formulário de login */}
-      <Layout style={styles.containerForm}>
-        <Formik
-          initialValues={{ username: '', password: '', keepConnected: false }}
-          validationSchema={LoginSchema}
-          onSubmit={(value) => onSubmitting(value)}
-        >
-          {
-            ({ handleChange, handleBlur, handleSubmit, values, setFieldValue, errors, touched }) => (
-              <Layout>
-                <Layout style={styles.containerInput}>
-                  <Input
-                    placeholder="Seu e-mail"
-                    returnKeyType="next"
-                    onSubmitEditing={() => passwordRef.current?.focus()}
-                    autoCapitalize="none"
-                    value={values.username}
-                    onChangeText={handleChange('username')}
-                    onBlur={handleBlur('username')}
-                    status={touched.username ? "danger" : "basic"}
-                  />
-                  {touched.username ? <Text status="danger">{errors.username}</Text> : null}
+        {/* Container para o formulário de login */}
+        <Layout style={styles.containerForm}>
+          <Formik
+            initialValues={{ username: '', password: '', keepConnected: false }}
+            validationSchema={LoginSchema}
+            onSubmit={(value) => onSubmitting(value)}
+          >
+            {
+              ({ handleChange, handleBlur, handleSubmit, values, setFieldValue, errors, touched }) => (
+                <Layout>
+                  <Layout style={styles.containerInput}>
+                    <Input
+                      placeholder="Seu e-mail"
+                      returnKeyType="next"
+                      onSubmitEditing={() => passwordRef.current?.focus()}
+                      autoCapitalize="none"
+                      value={values.username}
+                      onChangeText={handleChange('username')}
+                      onBlur={handleBlur('username')}
+                      status={touched.username ? "danger" : "basic"}
+                    />
+                    {touched.username ? <Text status="danger">{errors.username}</Text> : null}
+                  </Layout>
+
+                  <Layout style={styles.containerInput}>
+                    <Input
+                      placeholder="Sua senha"
+                      secureTextEntry
+                      ref={passwordRef}
+                      autoCapitalize="none"
+                      returnKeyType="done"
+                      value={values.password}
+                      onChangeText={handleChange('password')}
+                      onBlur={handleBlur('password')}
+                      status={touched.password ? "danger" : "basic"}
+                    />
+                    {touched.password ? <Text status="danger">{errors.password}</Text> : null}
+                  </Layout>
+
+
+                  <Layout style={styles.containerCheckForgot}>
+                    <CheckBox
+                      checked={values.keepConnected}
+                      onChange={(value) => setFieldValue('keepConnected', value)}
+                    >
+                      Mantenha-me conectado
+                    </CheckBox>
+                    <Button appearance="ghost" status="basic" size="small" onPress={() => navigation.navigate("ForgotPassword")}>
+                      Esqueci minha senha
+                    </Button>
+                  </Layout>
+
+                  <Button style={styles.button} size="medium" onPress={handleSubmit as any} disabled={loading}>{loading ? "Entrando..." : "Entrar"}</Button>
                 </Layout>
 
-                <Layout style={styles.containerInput}>
-                  <Input
-                    placeholder="Sua senha"
-                    secureTextEntry
-                    ref={passwordRef}
-                    autoCapitalize="none"
-                    returnKeyType="done"
-                    value={values.password}
-                    onChangeText={handleChange('password')}
-                    onBlur={handleBlur('password')}
-                    status={touched.password ? "danger" : "basic"}
-                  />
-                  {touched.password ? <Text status="danger">{errors.password}</Text> : null}
-                </Layout>
+              )
+            }
+          </Formik>
+        </Layout>
 
+        <Layout style={styles.containerFooter}>
+          <Button
+            appearance='outline'
+            status="danger"
+            size="small"
+            style={styles.button}
+            onPress={() => navigation.navigate("Register")}
+          >
+            Cadastrar-me
+          </Button>
+        </Layout>
 
-                <Layout style={styles.containerInput}>
-                  <CheckBox
-                    checked={values.keepConnected}
-                    onChange={(value) => setFieldValue('keepConnected', value)}
-                  >
-                    Mantenha-me conectado
-                  </CheckBox>
-                </Layout>
-
-                <Button style={styles.button} onPress={handleSubmit as any} disabled={loading}>{loading ? "Entrando..." : "Entrar"}</Button>
-              </Layout>
-
-            )
-          }
-        </Formik>
-      </Layout>
-
-      <Layout style={styles.containerFooter}>
-        <Button
-          appearance='outline'
-          status="success"
-          style={styles.button}
-          onPress={() => navigation.navigate('ForgotPassword')}
-        >
-          Esqueci minha senha
-        </Button>
-
-        <Button
-          appearance='outline'
-          status="danger"
-          style={styles.button}
-          onPress={() => navigation.navigate("Register")}
-        >
-          Cadastrar-me
-        </Button>
-      </Layout>
-    </Layout>
+        </Layout>
+      </ScrollView>
+    </KeyboardAvoidingView>
   );
 }
--- src/screens/auth/login/styles.tsx
+++ src/screens/auth/login/styles.tsx
@@ -3,32 +3,48 @@ import { StyleService } from "@ui-kitten/components";
 const styles = StyleService.create({
     container: {
         flex: 1,
-        width: "100%",
-        alignItems: "center",
-        justifyContent: "space-between",
+        flexDirection: 'column',
         paddingHorizontal: 10,
-        
+        paddingTop: 48,
+        alignItems: "center",
+        justifyContent: "space-between",       
     },
     containerImg:{
         width: "100%",
-        height: "25%",
+        height: "18%",
         alignItems: "center",
-        justifyContent: 'center',
+        justifyContent: 'flex-start',
     },
     containerForm: {
         width: "100%",
+        height: "40%",
+        justifyContent: "space-around",
     },
     containerInput: {
         marginBottom: 20,
     },
     containerFooter: {
         width: "100%",
-        height: "15%",
+        paddingTop: 10,
         justifyContent: "space-around",
+        borderTopWidth: 1,
+        borderColor: "#ccc",
+    },
+    containerCheckForgot: {
+      flexDirection: "row",
+      justifyContent: "space-between",
+      height: 50,  
+      marginBottom: 10,
     },
     button: {
-        borderRadius: 15,
+        borderRadius: 20,
+        marginBottom: 20,
     },  
+    image: {
+        width: "60%",
+        height: "100%",
+        resizeMode: "contain",
+    },
 });
 
 export default styles
