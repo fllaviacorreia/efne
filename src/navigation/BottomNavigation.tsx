6bd37f28f71efd278ac336b6c1f0d0369564af15
Flávia Correia <fllaviacorreia@outlook.com>
2025-01-01 13:18:15 -0300

    config navigators

--- src/navigation/BottomNavigation.tsx
+++ src/navigation/BottomNavigation.tsx
@@ -1,28 +1,71 @@
 // NAVIGATORS
-import DetailsScreen from "@/screens/details/DetailsScreen";
-import HomeScreen from "@/screens/athetes/HomeScreen";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-
-import { Icon } from '@ui-kitten/components';
+import Octicons from '@expo/vector-icons/Octicons';
 
 // TEXT TO BOTTOM
 import { Text } from "@ui-kitten/components";
 
-const TabIcon = (props: any) => <Icon {...props} name={props.name} />;
+// SCREENS
+import HomeScreen from "@/screens/athetes/AthletesScreen";
+import DetailsAthleteScreen from "@/screens/athetes/DetailsAthleteScreen";
+
+const TabIcon = (props: any) => <Octicons {...props} name={props.name} />;
 
 const Tab = createBottomTabNavigator();
 
-const HomeStack = createNativeStackNavigator();
+const AthletesStack = createNativeStackNavigator();
+const CategoriesStack = createNativeStackNavigator();
+const FinancialStack = createNativeStackNavigator();
+const FrequenciesStack = createNativeStackNavigator();
+const SettingsStack = createNativeStackNavigator();
 
-function HomeStackScreen() {
+function AthletesStackScreen() {
   return (
-    <HomeStack.Navigator screenOptions={{ headerShown: false }}>
-      <HomeStack.Screen name="Home" component={HomeScreen} />
-      <HomeStack.Screen name="Details" component={DetailsScreen} />
-      <HomeStack.Screen name="NewAthlete" component={HomeScreen} />
-    </HomeStack.Navigator>
+    <AthletesStack.Navigator screenOptions={{ headerShown: false }}>
+      <AthletesStack.Screen name="Athletes" component={HomeScreen} key="ListAthletes"/>
+      <AthletesStack.Screen name="Details" component={DetailsAthleteScreen} />
+      <AthletesStack.Screen name="NewAthlete" component={HomeScreen} />
+    </AthletesStack.Navigator>
   );
+}
+
+function CategoriesStackScreen() {
+  return (
+    <CategoriesStack.Navigator screenOptions={{headerShown: false}}>
+      <CategoriesStack.Screen name="Categories" component={HomeScreen} options={{ headerShown: false }} key="ListCategories" />
+      <CategoriesStack.Screen name="Details" component={HomeScreen} options={{ headerShown: false }} key="CategoriesDetails" />
+      <CategoriesStack.Screen name="NewCategory" component={HomeScreen} options={{ headerShown: false }} key="NewCategory" />
+    </CategoriesStack.Navigator>
+  )
+}
+
+function FinancialStackScreen() {
+  return (
+    <FinancialStack.Navigator screenOptions={{headerShown: false}}>
+      <FinancialStack.Screen name="Financials" component={HomeScreen} options={{ headerShown: false }} key="ListFinancials" />
+      <FinancialStack.Screen name="Details" component={HomeScreen} options={{ headerShown: false }} key="CategoriesDetails" />
+      <FinancialStack.Screen name="NewPayment" component={HomeScreen} options={{ headerShown: false }} key="NewPayment" />
+    </FinancialStack.Navigator>
+  )
+}
+
+function FrequenciesStackScreen() {
+  return (
+    <FrequenciesStack.Navigator screenOptions={{headerShown: false}}>
+      <FrequenciesStack.Screen name="Frequencies" component={HomeScreen} options={{ headerShown: false }} key="ListFrequencies" />
+      <FrequenciesStack.Screen name="Details" component={HomeScreen} options={{ headerShown: false }} key="CategoriesDetails" />
+      <FrequenciesStack.Screen name="NewFrequency" component={HomeScreen} options={{ headerShown: false }} key="NewFrequency" />
+    </FrequenciesStack.Navigator>
+  )
+}
+
+function SettingsStackScreen() {
+  return (
+    <SettingsStack.Navigator screenOptions={{headerShown: false}}>
+      <SettingsStack.Screen name="Settings" component={HomeScreen} options={{ headerShown: false }} key="Settings" />
+    </SettingsStack.Navigator>
+  )
 }
 // rest of stacks screens
 
@@ -41,15 +84,15 @@ export function Tabs() {
           let iconName;
 
           if (route.name === 'Athletes') {
-            iconName = 'people-outline';
+            iconName = 'people';
           } else if (route.name === 'Categories') {
-            iconName = 'grid-outline';
+            iconName = 'tag';
           } else if (route.name === 'Financial') {
-            iconName = 'credit-card-outline';
+            iconName = 'book';
           } else if (route.name === 'Frequencies') {
-            iconName = 'calendar-outline';
+            iconName = 'checklist';
           } else if (route.name === 'Settings') {
-            iconName = 'settings-outline';
+            iconName = 'gear';
           }
 
           return (
@@ -66,10 +109,42 @@ export function Tabs() {
     >
       <Tab.Screen
         name="Athletes"
-        component={HomeStackScreen}
+        component={AthletesStackScreen}
         options={{
           tabBarLabel: ({ color }) =>
             <Text style={{ fontSize: 12, color: color }}>Atletas </Text>
+        }}
+      />
+      <Tab.Screen
+        name="Categories"
+        component={CategoriesStackScreen}
+        options={{
+          tabBarLabel: ({ color }) =>
+            <Text style={{ fontSize: 12, color: color }}>Categorias</Text>
+        }}
+      />
+      <Tab.Screen
+        name="Financial"
+        component={FinancialStackScreen}
+        options={{
+          tabBarLabel: ({ color }) =>
+            <Text style={{ fontSize: 12, color: color }}>Financiamentos</Text>
+        }}
+      />
+      <Tab.Screen
+        name="Frequencies"
+        component={FrequenciesStackScreen}
+        options={{
+          tabBarLabel: ({ color }) =>
+            <Text style={{ fontSize: 12, color: color }}>Frequencias</Text>
+        }}
+      />
+      <Tab.Screen
+        name="Settings"
+        component={SettingsStackScreen}
+        options={{
+          tabBarLabel: ({ color }) =>
+            <Text style={{ fontSize: 12, color: color }}>Configurações</Text>
         }}
       />
     </Tab.Navigator>
