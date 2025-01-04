46a149f4a07506ebcc59fe009d0fc375212b8bf6
Flávia Correia <fllaviacorreia@outlook.com>
2025-01-04 15:43:42 -0300

    fix .env

--- env.d.ts
+++ env.d.ts
@@ -6,6 +6,7 @@ declare module '@env' {
     export const FIREBASE_MESSAGING_SENDER_ID: string;
     export const FIREBASE_APP_ID: string;
     export const FIREBASE_MEASUREMENT_ID: string;
+    export const TEXT_TO_TEST: string;
     // Adicione outras variáveis conforme necessário
   }
   
